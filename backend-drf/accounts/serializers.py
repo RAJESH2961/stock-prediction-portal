72a8358ba89f597ed99fb9afa70f3f7d462636da
Rajesh <grajesh2906@gmail.com>
2025-05-31 19:42:13 +0530

    added serializers.py for default account creation model

---
+++ backend-drf/accounts/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    # Creating an user with an built in method
+    def create(self, validated_data):
+        # User.objects.create = save the password in a plain text
+        # User.objects.create = automatically hash the password
+        user = User.objects.create_user(
+            validated_data['username'],
+            validated_data['email'],
+            validated_data['password'],
+        )
+        # Method - 2 
+        # user = User.objects.create_user(**validated_data)
+        return user
