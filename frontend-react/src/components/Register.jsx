befe4457bdc13d9fada073d581894016b09d554e
Rajesh <grajesh2906@gmail.com>
2025-05-31 23:49:24 +0530

    Added confetti effect after successfull login

--- frontend-react/src/components/Register.jsx
+++ frontend-react/src/components/Register.jsx
@@ -1,9 +1,19 @@
-import React, { useState } from 'react';
+import React, { use, useState } from 'react';
 import './Register.css';
 import axios from 'axios'
 
+// confetti effet
+import confetti from 'canvas-confetti';
+
+
 const Register = () => {
   const [form, setForm] = useState({ username: '', email: '', password: '' });
+
+    // Handling Errors
+    // Error can be user name aldready exist
+    // password is short
+    const [errors, setErrors] = useState({});
+    // const [success, setSuccess] = useState(false);
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -16,13 +26,21 @@ const Register = () => {
   try {
     const response = await axios.post('http://localhost:8000/api/v1/register/', form);
     console.log("Response data:", response.data);
-    alert("Registration successful!");
+    // alert("Registration successful!");
+    // set the error to blank once success
+    setErrors({});
+    // set success
+    // setSuccess(true);
+
+    // Launching confetti effect
+    confetti(); // 🎉 Launch confetti on success
     // Optionally redirect or clear form
     setForm({ username: '', email: '', password: '' });
   } catch (error) {
+    setErrors(error.response.data)
     if (error.response) {
       console.error("Registration Error:", error.response.data);
-      alert(`Error: ${JSON.stringify(error.response.data)}`);
+    //   alert(`Error: ${JSON.stringify(error.response.data)}`);
     } else {
       console.error("Error:", error.message);
     }
@@ -46,6 +64,7 @@ const Register = () => {
             onChange={handleChange}
             required
           />
+          <small>{errors.username && <div className='text-danger'>{errors.username}</div>}</small>
         </div>
 
         <div className="form-group">
@@ -59,6 +78,7 @@ const Register = () => {
             onChange={handleChange}
             required
           />
+          <small>{errors.email && <div className='text-danger'>{errors.email}</div>}</small>
         </div>
 
         <div className="form-group">
@@ -72,7 +92,9 @@ const Register = () => {
             onChange={handleChange}
             required
           />
+          <small>{errors.password && <div className='text-danger'>{errors.password}</div>}</small>
         </div>
+        {/* {success && <div className='alert alert-success'>Registration Successful</div>} */}
 
         <button type="submit" className="submit-btn">Register</button>
       </form>
